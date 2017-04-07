ec2eafde518129935a4f315551b60349206da272
Ian Hinden <ianhinden@Ians-iMac.local>
2017-04-06 23:47:27 -0700

    Created database

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:test123@localhost/ipfree'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
+
+class IP(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	ip_address = db.Column(db.String, unique=True)
+	free = db.Column(db.Boolean)
+
+	def __init__(self, ip_address, free):
+		self.ip_address = ip_address
+		self.free = free
+
+	def __repr__(self):
+		return '<IP %r>' % self.ip_address
 
 @app.route('/')
 def index():
