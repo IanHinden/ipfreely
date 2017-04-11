948e1c210e04406299e60a03ebcec300460194e6
System Administrator <root@Ians-iMac.local>
2017-04-10 22:13:05 -0700

    Add index template

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask import render_template
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:test123@localhost/ipfree'
@@ -20,7 +21,7 @@ class IP(db.Model):
 
 @app.route('/')
 def index():
-	return "hello flask"
+	return render_template('index.html')
 
 if __name__ == "__main__":
 	app.run()
