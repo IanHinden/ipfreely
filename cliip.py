4542e80dedaa91c56408d339985792a320d67fa1
System Administrator <root@Ians-iMac.local>
2017-04-13 22:55:44 -0700

    Design front end

--- cliip.py
+++ cliip.py
@@ -21,8 +21,8 @@ network = ipaddress.IPv4Network(uMySubnet , strict=False)
 for x in network.hosts():
 	response = pyping.ping(str(x))
 	ip = socket.inet_ntoa(struct.pack('!L', x))
-	ip_record = IpStatus.query.filter(IpStatus.ip_address.endswith(ip)) #see if there is an equals method instead of endswith
 	status = IpStatus(ip, True)
+	ip_record = IpStatus.query.filter_by(ip_address=ip).first()
 	if response.ret_code == 0:
 		print "%s is free" % (ip)
 		status.free = True
